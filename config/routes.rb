0e28a97c4331ef853514b408b0a01191bcedaf9e
Hakeem <babahakeem@outlook.com>
2020-08-24 21:10:06 +0200

    basic controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'football_teams#index'
+
+  get 'football_teams', to: "football_teams#index"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
