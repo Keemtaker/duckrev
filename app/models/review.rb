26bd5a7cf2d980d712999d85d2b0f84f1e8de2cc
Hakeem <babahakeem@outlook.com>
2020-10-03 13:26:41 +0200

    uniq validation on review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :football_score
+
+  validates :football_score_id, uniqueness: { scope: :user_id, message: "You've reviewed this score already!" }
 end
