adf670fab70f1088e2a8662c4bd11b6121f3e6c7
Hakeem <babahakeem@outlook.com>
2020-10-05 23:27:22 +0200

    basic design for football teams

--- app/controllers/football_teams_controller.rb
+++ app/controllers/football_teams_controller.rb
@@ -7,5 +7,7 @@ class FootballTeamsController < ApplicationController
   def create
     @user = current_user
     @user.football_team = FootballTeam.find(params[:team])
+    flash[:notice] = "You have chosen #{@user.football_team.short_name} as your club"
+    redirect_to football_scores_path
   end
 end
