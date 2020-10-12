a92263ced6d0b6eac0c87f095d1ca3211cfdd46b
Hakeem <babahakeem@outlook.com>
2020-10-12 20:51:29 +0200

    setup of basic functional tests

--- test/controllers/football_scores_controller_test.rb
+++ test/controllers/football_scores_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class FootballScoresControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @first_score = football_scores(:FirstScore)
+    @first_user = users(:FirstUser)
+  end
+
+  test "should get index" do
+    sign_in @first_user
+    get football_scores_url
+    assert_response :success
+  end
+
+  test "should still get index if user is not signed in" do
+    get football_scores_url
+    assert_response :success
+  end
+
+  test "should get show page" do
+    get football_score_url(@first_score)
+    assert_response :success
+  end
+
+  #View tests
+  test "for elements on index" do
+    sign_in @first_user
+    get football_scores_url
+    assert_select "div", @first_score.competition_name
+    assert_select 'a'
+  end
+
+  test "for elements on show" do
+    get football_scores_url
+    assert_select "div", @first_score.competition_name
+    assert_select 'a'
+  end
+
 end
