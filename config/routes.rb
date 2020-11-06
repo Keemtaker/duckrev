2012d0c204ba9ee102cb0097382a186355f860af
Hakeem <babahakeem@outlook.com>
2020-11-06 15:31:39 +0100

    about page setup

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :football_scores do
     resources :football_reviews, only: [:new, :create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'about', to: "pages#about"
+
 end
