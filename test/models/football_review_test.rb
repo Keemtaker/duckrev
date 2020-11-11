fbf646006a545029ecc39e37be814ed5905d9a9b
Hakeem <babahakeem@outlook.com>
2020-11-11 23:19:38 +0100

    model test for archived score

--- test/models/football_review_test.rb
+++ test/models/football_review_test.rb
@@ -4,7 +4,10 @@ class FootballReviewTest < ActiveSupport::TestCase
 
   def setup
     @first_review = football_reviews(:FirstReview)
-    @second_user = users(:SecondUser)
+    @first_score = football_scores(:FirstScore)
+    @second_score = football_scores(:SecondScore)
+    @third_user = users(:ThirdUser)
+
   end
 
   test "football review is not valid with rating outside 1..10" do
@@ -15,6 +18,12 @@ class FootballReviewTest < ActiveSupport::TestCase
   test "football review is not valid with content over the maximum" do
     @first_review.content = "#{@first_review.content}" * 100
     assert @first_review.invalid?
+  end
+
+  test "football review is not valid with archived score" do
+    new_review = FootballReview.new(content: "amazing game by the lads", rating: 2, football_score_id: @second_score.id, user_id: @third_user.id )
+    new_review.football_score.archived_state = true
+    assert new_review.invalid?
   end
 
   test "user can review a score just once" do
