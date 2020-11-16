91a7be62927fef90e025e5de77b686bd59c53d1f
Hakeem <babahakeem@outlook.com>
2020-11-16 20:59:56 +0100

    first sign in should redirect to football teams path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if resource.sign_in_count == 1
+      football_teams_path
+    else
+      stored_location_for(resource) || signed_in_root_path(resource)
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_175547) do
+ActiveRecord::Schema.define(version: 2020_11_16_172604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,11 @@ ActiveRecord::Schema.define(version: 2020_10_27_175547) do
     t.string "location"
     t.string "access_secret"
     t.string "access_token"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
