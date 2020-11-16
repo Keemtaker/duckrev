351195a875e3e132af2c577a261998845b1602bc
Hakeem <babahakeem@outlook.com>
2020-11-16 22:33:22 +0100

    added permitted params

--- app/controllers/football_teams_controller.rb
+++ app/controllers/football_teams_controller.rb
@@ -6,8 +6,15 @@ class FootballTeamsController < ApplicationController
 
   def create
     @user = current_user
-    @user.football_team = FootballTeam.find(params[:team])
+    @user.football_team = FootballTeam.find(football_team_params)
     flash[:notice] = "You have chosen #{@user.football_team.short_name} as your club"
     redirect_to football_scores_path
   end
+
+  private
+
+  def football_team_params
+    params.require(:team)
+  end
+
 end
